90cd056091c95d4e1efa3ff6c373fd58a98acbe2
Simon Konstantyner <simon@konstantyner.dk>
2019-06-28 15:46:28 +0200

    changes to template

--- template/Main.java
+++ template/Main.java
@@ -1,7 +1,22 @@
 package template;
 
+import template.pizza.*;
+
 public class Main {
     public static void main(String[] args) {
+        pizzaExample();
+    }
+
+    public static void pizzaExample() {
+        Pizza a = new CheesePizza();
+        Pizza b = new PepperoniPizza();
+
+        a.prepare();
+        System.out.println("");
+        b.prepare();
+    }
+
+    public static void pageExample() {
         PageTemplate a = new PageA();
         PageTemplate b = new PageB();
 
